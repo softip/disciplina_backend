f791940eb838df88ec4a0c1857c9c597168a56c1
Ivan Paulino Pereira <07298764624@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-06 09:20:43 -0300

    Correção da Conexão

--- projeto/banco/conexao.php
+++ projeto/banco/conexao.php
@@ -4,15 +4,15 @@
   $usuario = "root";
   $senha = "";
   $banco = "db_3infob";
+  $porta = 3366;
 
   //cria uma conexão com o banco de dados no mysql
-  $conexao = new mysqli($maquina, 
-                        $usuario, $senha,  $banco);
+  $conexao = new mysqli($maquina, $usuario, $senha, $banco, $porta);
   
   //verifica se houve erros na conexão
   if($conexao->connect_error){
       echo $conexao->connect_error;
-     die(); 
+      die(); 
   }
 
 
